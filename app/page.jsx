f3b6f9ce4185dc2be36bd09b68ddedf2072d1ec4
dl00063 <DLlensa.Globant@supervielle.com.ar>
2023-07-03 22:04:30 -0300

    develop Added welcome message on home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="bg-gray-100 py-8">
+				<div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+					<h2 className="text-3xl font-extrabold text-gray-900 mb-4">
+						Welcome to NASA!
+					</h2>
+					<p className="text-lg text-gray-700">
+						Explore the wonders of space and the mysteries of the universe.
+					</p>
+				</div>
+			</div>
+		</main>
 	);
 }
